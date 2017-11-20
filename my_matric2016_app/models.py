a99045d5fa5cf6df3ee05afa2ec067cc70691ae1
Sanele13 <aynstyn@gmail.com>
2017-11-20 18:31:08 +0200

    Model Creation
    I have created a model for my app. I have  created all the fields that are necessary for the representation of the data.

--- my_matric2016_app/models.py
+++ my_matric2016_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class matric_app_model(models.Model):
+    emis = models.TextField(max_length=100)
+	centre_no = models.TextField(max_length=100)
+	name = models.TextField(max_length=100)
+	wrote_2014 = models.TextField(max_length=100)
+	passed_2014 = models.TextField(max_length=100)
+	wrote_2015 = models.TextField(max_length=100)
+	passed_2015 = models.TextField(max_length=100)
+	wrote_2016 = models.TextField(max_length=100)
+	passed_2016 = models.TextField(max_length=100)
+	
+	#add an extra field for 'province'
+	province = models.TextField(max_length = 2)
--- my_matric2016_app/views.py
+++ my_matric2016_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+#hello world!
 def HelloWorld(request):
     text = "<h1>Hello World!</h1>"
     return HttpResponse(text)
