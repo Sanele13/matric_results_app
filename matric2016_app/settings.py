a30bc19d2d9f939298fef2016fd0cb4918f8e4bd
Sanele13 <aynstyn@gmail.com>
2017-11-20 21:55:44 +0200

    Changed the Django database to a local MySQL database

--- matric2016_app/settings.py
+++ matric2016_app/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'matric2016_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+		'USER':'root',
+		'PASSWORD':'zukiswa',
+		'HOST':'127.0.0.1',
+		'PORT':'3306',
     }
 }
 
