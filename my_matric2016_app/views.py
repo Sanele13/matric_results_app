05f24f29fdd2f45e4f09dade6ac0637e500e323d
Sanele13 <aynstyn@gmail.com>
2017-11-22 19:17:22 +0200

    Added functionality to get data from database and display it

--- my_matric2016_app/views.py
+++ my_matric2016_app/views.py
@@ -4,6 +4,7 @@ from rest_framework import generics
 from .models import matric_app_model
 from .serializers import matric2016_serializer
 import csv
+import psycopg2
 
 # Create your views here.
 
@@ -18,22 +19,49 @@ class APIView(generics.ListCreateAPIView):
 	
     def perform_create(self, serializer):
         serializer.save()
+#connect to DB
 
 def index(request):
-    #read and display DB content
-    
-	#get every DB entry
-    #entries = matric_app_model.objects.all()
+    conn = psycopg2.connect("host='ec2-23-21-101-174.compute-1.amazonaws.com' user='rxtgqfxfcogpjk' password='d5644497df9db5ba0662be23d640c1dab93d7bfb4aad01cc23c3061bcb4ba2fa' dbname='d338a782pecr9b'")   
+    cur=conn.cursor()
+#get number of students who wrote and those who passed, given 'year' and 'province'
+    def getData(year,province):
+        cur.execute("select wrote_"+year+",passed_"+year+" from test_table where province='"+province+"'")
+        rows=cur.fetchall()
+        return rows
+
+
+    def num_wrote(data):
+        sum=0
+        for n in data:
+        #print(n[0])
+            if(str(n[0])!=''):
+                sum+=int(str(n[0]))
+            else:
+                sum+=0 
+        return sum
+	
+    def num_passed(data):
+        sum=0
+        for n in data:
+        #print(n[0])
+            if(str(n[1])!=''):
+                sum+=int(str(n[1]))
+            else:
+                sum+=0 
+        return sum
+	#Displaying graphs and things
     head = "<html><head><title>Matric Results</title><script src = 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js'></script><link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'></head>"
     body = "<body><div style = 'width:40%;height:40%'><canvas id = 'chart'></canvas></div><script>var myChart = document.getElementById('chart').getContext('2d');var barChart = new Chart(myChart, {type: 'bar',data:{labels:['EC','WC','KZ','LP','NC','NW','FS','MP','GA'],"
-    datasets="datasets:[{label:'Wrote',data:[70,80,74,95,82,49,78,92,68],backgroundColor:'black',},{label:'Passed',data:[60,70,52,81,69,49,43,58,49],backgroundColor:'green'},]},"
+    datasets="datasets:[{label:'Wrote',data:["+str(num_wrote(getData('2014','EC')))+","+str(num_wrote(getData('2014','WC')))+",74,95,82,49,78,92,68],backgroundColor:'black',},{label:'Passed',data:[60,70,52,81,69,49,43,58,49],backgroundColor:'green'},]},"
     rest ="options:{scales:{xAxes:[{scaleLabel:{display:true,labelString: 'Provinces'}}],yAxes:[{scaleLabel:{display:true,labelString: 'Number of Students'}}]}}})</script></body></html>"
 	
     content = head+body+datasets+rest
-    #for entry in entries:
-    #    content = content + entry.name + "<br>"
+    conn.close() #close connection
     return HttpResponse(content)
+    	
 
+#I no longer need this method!
 def loadCSVdata(request):
     with open('2016_Matric_Schools_Report.csv') as csvfile:
         CSVfile = csv.reader(csvfile, delimiter = ',')
