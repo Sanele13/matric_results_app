e1a985e8f754a0c0284c16896e0836caea2efc84
Sanele13 <aynstyn@gmail.com>
2017-11-22 10:25:40 +0200

    I can now read provinces. Now it's time to add to database.

--- loadCSVdata.py
+++ loadCSVdata.py
@@ -2,7 +2,7 @@ import csv
     
 file1 = open('2016_Matric_Schools_Report.csv')
 file2 = open('National Ordinary schools.csv')
-csv1 = csv.reader(file1)
+csv1 = csv.reader(file1,delimiter=',')
 csv2 = csv.reader(file2, delimiter=',')
 next(csv1, None)	
 next(csv2, None)	
@@ -10,8 +10,11 @@ data1 = list(csv1)
 data2 = list(csv2)
 print(len(data1))
 print(len(data2))
-
+num=0
 for i in range(0,len(data1)):
     for j in range(0,len(data2)):
         if(data1[i][0]==data2[j][0]):
-            print(data1[i][0]+" "+data2[j][3])
+            print((data1[i][0]+" "+data1[i][1]+" "+data1[i][2]+" "+data1[i][3]+" "+data1[i][4]+" "+data1[i][5]+" "+data1[i][6]+" "+data1[i][7]+" "+data1[i][8]+" "+data2[j][3]).encode('utf-8'))
+            num+=1
+    print(i)	
+print(num)
--- my_matric2016_app/serializers.py
+++ my_matric2016_app/serializers.py
@@ -4,4 +4,4 @@ from .models import matric_app_model
 class matric2016_serializer(serializers.ModelSerializer):
     class Meta:
         model = matric_app_model
-        fields = ('id', 'emis', 'centre_no', 'name','wrote_2014','passed_2014','wrote_2015','passed_2015','wrote_2016','passed_2016')
+        fields = ('id', 'emis', 'centre_no', 'name','wrote_2014','passed_2014','wrote_2015','passed_2015','wrote_2016','passed_2016','province')
