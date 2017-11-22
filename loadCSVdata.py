825ad01962f6758eeb1803dd50430ed6dcfc604a
Sanele13 <aynstyn@gmail.com>
2017-11-22 02:15:55 +0200

    Good progress
    I am able to read two CSV files. One of these has a province field in it. This field will be very useful in displaying data.

--- loadCSVdata.py
+++ loadCSVdata.py
@@ -1,15 +1,7 @@
 import csv
-
-with open('file2.csv') as csvfile2:
-    csv2 = csv.reader(csvfile2, delimiter=',')
-    next(csv2, None)	
-with open('file1.csv') as csvfile1:
-    csv1 = csv.reader(csvfile1, delimiter=',')
-    	
-    next(csv1, None)
     
-file1 = open('file1.csv')
-file2 = open('file2.csv')
+file1 = open('2016_Matric_Schools_Report.csv')
+file2 = open('National Ordinary schools.csv')
 csv1 = csv.reader(file1)
 csv2 = csv.reader(file2, delimiter=',')
 next(csv1, None)	
@@ -17,6 +9,9 @@ next(csv2, None)
 data1 = list(csv1)
 data2 = list(csv2)
 print(len(data1))
+print(len(data2))
+
 for i in range(0,len(data1)):
     for j in range(0,len(data2)):
-        print(data1[i][0]+" "+data2[j][1])
+        if(data1[i][0]==data2[j][0]):
+            print(data1[i][0]+" "+data2[j][3])
